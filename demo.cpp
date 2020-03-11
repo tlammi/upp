b4762d3b974476a157f06c8caa0da7c126994226
tlammi <tangojulietlima@gmail.com>
2020-03-11 13:43:50 +0200

    Cleaner ownership of the parsing data

--- demo.cpp
+++ demo.cpp
@@ -3,9 +3,9 @@ using namespace upp::cli;
 
 int main(int argc, const char** argv) {
     Parser<int> p(nullptr, nullptr, "Demo program");
-    p.boolopts().add('f', "flag", "Demo flag");
-    p.opts().add('a', "aaaaa", "AAAA option");
-    p.vectopts().add('v', "vector-option", "This is a vector option");
-    p.opts().add("no-short", "This flag has no short version");
+    p.add_bool_option('f', "flag", "Demo flag");
+    p.add_option('a', "aaaaa", "AAA option");
+    p.add_vector_option('v', "vector-option", "This is a vector option");
+    p.add_option("no-short", "This option has no short flag");
     p.parse(argc, argv);
 }
--- include/upp/cli/parser.hpp
+++ include/upp/cli/parser.hpp
@@ -14,6 +14,13 @@ namespace cli {
 template <typename T>
 class Parser {
  public:
+    struct ParsingData {
+        OptContainer<bool> bool_options;
+        OptContainer<Value> options;
+        OptContainer<VectValue> vector_options;
+        VectValue positional_arguments;
+    };
+
     typedef int (*callback_t)(T*);
 
     explicit Parser(callback_t callback = nullptr, T* parg = nullptr,
@@ -21,31 +28,49 @@ class Parser {
         : _cback{callback},
           _parg{parg},
           _helpstr{helpstr},
-          _boolopts{},
-          _opts{},
-          _vectopts{},
+          _parsing_data{},
           _subparsers{},
-          _parsed_subcmd{""},
           _pos_args{} {
-        _boolopts.add('h', "help", "Print this help and exit");
+        _parsing_data.bool_options.add('h', "help", "Print this help and exit");
     }
 
-    OptContainer<bool>& boolopts() { return _boolopts; }
-    OptContainer<Value>& opts() { return _opts; }
-    OptContainer<VectValue>& vectopts() { return _vectopts; }
-    std::map<std::string, Parser<T>>& subcommands() { return _subparsers; }
-    const std::string& parsed_subcmd() { return _parsed_subcmd; }
-    const VectValue& pos_args() const { return _pos_args; }
-
+    void add_bool_option(char shortflag, const std::string& longflag,
+                         const std::string& helpstr) {
+        _parsing_data.bool_options.add(shortflag, longflag, helpstr);
+    }
+    void add_bool_option(const std::string& longflag,
+                         const std::string& helpstr) {
+        _parsing_data.options.add(longflag, helpstr);
+    }
+    void add_option(char shortflag, const std::string& longflag,
+                    const std::string& helpstr) {
+        _parsing_data.options.add(shortflag, longflag, helpstr);
+    }
+    void add_option(const std::string& longflag, const std::string& helpstr) {
+        _parsing_data.options.add(longflag, helpstr);
+    }
+    void add_vector_option(char shortflag, const std::string& longflag,
+                           const std::string& helpstr) {
+        _parsing_data.vector_options.add(shortflag, longflag, helpstr);
+    }
+    void add_vector_option(const std::string& longflag,
+                           const std::string& helpstr) {
+        _parsing_data.vector_options.add(longflag, helpstr);
+    }
+    void add_subcommand(const std::string& name, const std::string& helpstr,
+                        callback_t, T* parg) {
+        (void)parg;
+        _subparsers[name] = Parser(nullptr, nullptr, helpstr);
+    }
     int parse(int argc, const char** argv) {
         std::vector<std::string> args{argv + 1, argv + argc};
         return parse(args);
     }
 
     int parse(const std::vector<std::string>& args) {
-        _boolopts.reset_values();
-        _opts.reset_values();
-        _vectopts.reset_values();
+        _parsing_data.bool_options.reset_values();
+        _parsing_data.options.reset_values();
+        _parsing_data.vector_options.reset_values();
         size_t i{0};
         while (i < args.size()) {
             std::string arg = args[i];
@@ -65,7 +90,6 @@ class Parser {
                 if (_subparsers.size()) {
                     std::vector<std::string> unparsed{
                         args.begin() + static_cast<int64_t>(i) + 1, args.end()};
-                    _parsed_subcmd = arg;
                     return _subparsers.at(arg).parse(unparsed);
                 } else {
                     _pos_args.push_back(arg);
@@ -91,16 +115,16 @@ class Parser {
 
     size_t _handle_sflags(char sflag, size_t curindex,
                           const std::vector<std::string>& args) {
-        if (_boolopts.shortflags().count(sflag)) {
-            _boolopts[sflag] = true;
+        if (_parsing_data.bool_options.shortflags().count(sflag)) {
+            _parsing_data.bool_options[sflag] = true;
             return curindex;
-        } else if (_opts.shortflags().count(sflag)) {
+        } else if (_parsing_data.options.shortflags().count(sflag)) {
             std::string val{args[curindex + 1]};
-            _opts[sflag] = val;
+            _parsing_data.options[sflag] = val;
             return curindex + 1;
-        } else if (_vectopts.shortflags().count(sflag)) {
+        } else if (_parsing_data.vector_options.shortflags().count(sflag)) {
             std::string val{args[curindex + 1]};
-            _vectopts[sflag].push_back(val);
+            _parsing_data.vector_options[sflag].push_back(val);
             return curindex + 1;
         }
         throw std::invalid_argument("Invalid short flag: " + args[curindex]);
@@ -108,16 +132,16 @@ class Parser {
 
     size_t _handle_lflags(const std::string& lflag, size_t curindex,
                           const std::vector<std::string>& args) {
-        if (_boolopts.longflags().count(lflag)) {
-            _boolopts[lflag] = true;
+        if (_parsing_data.bool_options.longflags().count(lflag)) {
+            _parsing_data.bool_options[lflag] = true;
             return curindex;
-        } else if (_opts.longflags().count(lflag)) {
+        } else if (_parsing_data.options.longflags().count(lflag)) {
             std::string val{args[curindex + 1]};
-            _opts[lflag] = val;
+            _parsing_data.options[lflag] = val;
             return curindex + 1;
-        } else if (_vectopts.longflags().count(lflag)) {
+        } else if (_parsing_data.vector_options.longflags().count(lflag)) {
             std::string val{args[curindex + 1]};
-            _vectopts[lflag].push_back(val);
+            _parsing_data.vector_options[lflag].push_back(val);
             return curindex + 1;
         }
         throw std::invalid_argument("Invalid long flag: " + args[curindex]);
@@ -129,7 +153,7 @@ class Parser {
         std::vector<std::tuple<std::string, std::string, std::string>>
             help_data{};
         std::vector<std::string> help_rows{};
-        auto tmp = _boolopts.help_data();
+        auto tmp = _parsing_data.bool_options.help_data();
         for (const auto& tup : tmp) {
             auto [s, l, h] = tup;
             long_width = std::max(long_width, l.size());
@@ -138,7 +162,7 @@ class Parser {
             else
                 help_data.push_back(std::make_tuple("", l, h));
         }
-        tmp = _opts.help_data();
+        tmp = _parsing_data.options.help_data();
         for (auto& tup : tmp) {
             long_width = std::max(long_width, std::get<1>(tup).size() + 6);
             auto [s, l, h] = tup;
@@ -149,7 +173,7 @@ class Parser {
             else
                 help_data.push_back(std::make_tuple("", l + " VALUE", h));
         }
-        tmp = _vectopts.help_data();
+        tmp = _parsing_data.vector_options.help_data();
         for (auto& tup : tmp) {
             auto [s, l, h] = tup;
             long_width = std::max(long_width, l.size() + 6);
@@ -164,7 +188,7 @@ class Parser {
 
         std::sort(help_data.begin(), help_data.end(),
                   [](const auto& a, const auto& b) {
-                      return std::get<2>(a) < std::get<2>(b);
+                      return std::get<1>(a) < std::get<1>(b);
                   });
 
         std::cerr << _helpstr << std::endl
@@ -185,11 +209,9 @@ class Parser {
     callback_t _cback;
     T* _parg;
     std::string _helpstr;
-    OptContainer<bool> _boolopts;
-    OptContainer<Value> _opts;
-    OptContainer<VectValue> _vectopts;
+
+    ParsingData _parsing_data;
     std::map<std::string, Parser<T>> _subparsers;
-    std::string _parsed_subcmd;
     VectValue _pos_args;
 };
 }  // namespace cli
