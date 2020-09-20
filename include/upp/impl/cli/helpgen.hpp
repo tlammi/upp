a1b3104a01cdd7a14979557110787532dde08d87
tlammi <tangojulietlima@gmail.com>
2020-09-20 09:00:50 +0300

    Documentation

--- include/upp/impl/cli/helpgen.hpp
+++ include/upp/impl/cli/helpgen.hpp
@@ -7,6 +7,19 @@
 #include "upp/impl/cli/opt.hpp"
 namespace upp {
 namespace cli {
+
+/**
+ * @brief Generate help for a command
+ *
+ * @param cmd Command for which the help is generated
+ * @param brief_help Brief help string. Short program description placed first
+ * in the help string
+ * @param usage Usage placed in the help print. E.g. <cmd> [options] [arguments]
+ *
+ * @param terminal_width Terminal width, If >0, use this as a guide for clean
+ * formatting. If <0, ignored
+ * @return std::string String containing the help text
+ */
 std::string helpgen(const Cmd& cmd, std::string brief_help = "",
 					std::string_view usage = "", int terminal_width = 0);
 
