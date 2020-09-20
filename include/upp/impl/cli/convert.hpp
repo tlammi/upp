bf7540dd9163b47533e74742208de6eafb2f3942
tlammi <tangojulietlima@gmail.com>
2020-09-20 08:55:51 +0300

    Simplified convert.hpp

--- include/upp/impl/cli/convert.hpp
+++ include/upp/impl/cli/convert.hpp
@@ -1,46 +1,46 @@
 #pragma once
 
+/**
+ * @file convert.hpp Conversion utils for string -> type conversion
+ * @author tlammi (tlammi@github.com)
+ * @brief
+ * @version 0.1
+ * @date 2020-09-20
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
 #include <sstream>
 #include <stdexcept>
 #include <string>
 
 #include "upp/impl/cli/enum.hpp"
 #include "upp/impl/cli/exception.hpp"
+
 namespace upp {
 namespace cli {
-
-template <typename T>
-struct ret_type {
-		using type = T;
-};
-
-template <typename T>
-struct ret_type<Enum<T>> {
-		using type = const char*;
-};
-
-template <typename T>
-using ret_type_t = typename ret_type<T>::type;
 
 template <auto A, typename...>
 auto asserting_value = A;
 
 template <typename T>
 struct converter {
-		static ret_type_t<T> convert(const char* str) {
-				if constexpr (std::is_same_v<std::string, ret_type_t<T>> ||
-							  std::is_same_v<const char*, ret_type_t<T>>) {
+		static T convert(const char* str) {
+				if constexpr (std::is_same_v<std::string, T> ||
+							  std::is_same_v<const char*, T>) {
 						return str;
-				} else if constexpr (std::is_floating_point_v<ret_type_t<T>>) {
+				} else if constexpr (std::is_floating_point_v<T>) {
 						return handle_floating(str);
-				} else if constexpr (std::is_integral_v<ret_type_t<T>>) {
+				} else if constexpr (std::is_integral_v<T>) {
 						return handle_integer(str);
 				} else {
 						static_assert(asserting_value<false, T>,
 									  "Unsupported conversion");
 				}
 		}
-		static ret_type_t<T> handle_floating(const char* str) {
+
+private:
+		static T handle_floating(const char* str) {
 				bool dot{false};
 				for (const char* c = str; *c != '\0'; ++c) {
 						if (*c == '.') {
@@ -56,12 +56,12 @@ struct converter {
 				}
 				std::stringstream ss;
 				ss << str;
-				ret_type_t<T> ret;
+				T ret;
 				ss >> ret;
 				return ret;
 		}
 
-		static ret_type_t<T> handle_integer(const char* str) {
+		static T handle_integer(const char* str) {
 				std::stringstream ss;
 				if (str[0] == '0' && str[1] == 'x') {
 						for (const char* c = &str[2]; *c != '\0'; ++c) {
@@ -80,12 +80,12 @@ struct converter {
 						}
 						ss << str;
 				}
-				ret_type_t<T> ret;
+				T ret;
 				ss >> ret;
 				return ret;
 		}
 
-		static void run_integer_checks(const ret_type_t<T>& val) {}
+		static void run_integer_checks(const T& val) {}
 };
 
 template <typename T>
