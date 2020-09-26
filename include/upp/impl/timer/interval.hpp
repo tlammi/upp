ad90c4551e15206e716d4922a277abe7977f6213
tlammi <tangojulietlima@gmail.com>
2020-09-26 19:50:51 +0300

    Added interval timer

---
+++ include/upp/impl/timer/interval.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <chrono>
+#include <condition_variable>
+#include <thread>
+
+namespace upp {
+namespace timer {
+template <typename Callable, typename Rep, typename Period>
+class Interval {
+public:
+		Interval(const std::chrono::duration<Rep, Period>& interval,
+				 Callable&& callable)
+			: f_{callable}, interval_{interval}, t_{[&]() { work(); }} {}
+
+		~Interval() {
+				{
+						std::unique_lock lk{mut_};
+						operate_ = false;
+				}
+				cv_.notify_one();
+				t_.join();
+		}
+
+private:
+		void work() {
+				while (true) {
+						std::unique_lock lk{mut_};
+						if (cv_.wait_for(lk, interval_,
+										 [&]() { return !operate_; })) {
+								break;
+						}
+						f_();
+				}
+		}
+		Callable f_;
+		const std::chrono::duration<Rep, Period> interval_;
+		std::mutex mut_{};
+		std::condition_variable cv_{};
+		std::thread t_;
+		bool operate_{true};
+};
+}  // namespace timer
+}  // namespace upp
---
+++ unit-test/timer/interval.cpp
@@ -0,0 +1,26 @@
+
+#include "upp/impl/timer/interval.hpp"
+
+#include <gtest/gtest.h>
+
+#include <atomic>
+
+using namespace upp::timer;
+using namespace std::literals::chrono_literals;
+
+TEST(IntervalTimer, Ctor) {
+		Interval i{100ms, []() {}};
+		Interval i2{100ms, []() {}};
+		Interval{10ms, []() {}};
+}
+
+TEST(IntervalTimer, Increment) {
+		std::atomic<int> i{0};
+
+		Interval t{10ms, [&]() { ++i; }};
+		Interval t1{20ms, [&]() { i += 2; }};
+
+		std::this_thread::sleep_for(100ms);
+
+		ASSERT_GT(i, 15);
+}
