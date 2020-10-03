5e132769eb22c2e4d814ec1c35549148fd77d985
tlammi <tangojulietlima@gmail.com>
2020-10-03 19:29:27 +0300

    Added specializations
    - Support for moving different Function specializations
      into each other
    - More unit tests

--- include/upp/function.hpp
+++ include/upp/function.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <algorithm>
+#include <cstddef>
 #include <new>
 #include <stdexcept>
 
@@ -27,59 +29,156 @@ struct CallableImpl<C, Ret, std::tuple<Args...>>
 		}
 		C c_;
 };
+
+constexpr size_t FUNCTION_SIZE_RESOLUTION = 32;
+
 }  // namespace detail
 
-template <typename C, size_t S = 32>
-struct Func {};
+template <typename C, size_t S = detail::FUNCTION_SIZE_RESOLUTION>
+struct Function {};
+
+namespace detail {
+template <typename T>
+struct is_function : public std::false_type {};
 
 template <typename Ret, size_t S, typename... Args>
-class Func<Ret(Args...), S> {
-public:
-		Func() {}
-		template <typename C>
-		Func(C&& c) : valid_{true} {
-				static_assert(
-					sizeof(detail::CallableImpl<C, Ret, std::tuple<Args...>>) <=
-						sizeof(buf_),
-					"Object too large to store in Func");
+struct is_function<Function<Ret(Args...), S>> : std::true_type {};
+}  // namespace detail
 
+template <typename Ret, size_t S, typename... Args>
+class Function<Ret(Args...), S> {
+		template <typename C, size_t S2>
+		friend class Function;
+
+public:
+		static_assert(S % detail::FUNCTION_SIZE_RESOLUTION == 0,
+					  "upp::Function size should be a multiple of 32");
+
+		Function() {}
+
+		template <typename C, typename Enable = std::enable_if_t<
+								  !detail::is_function<C>::value>>
+		Function(C&& c) {
+				constexpr size_t size =
+					sizeof(detail::CallableImpl<C, Ret, std::tuple<Args...>>);
+				static_assert(size <= sizeof(buf_),
+							  "Object too large to store in Func");
+				size_ = size;
 				new ((void*)buf_)
 					detail::CallableImpl<C, Ret, std::tuple<Args...>>(
 						std::forward<C>(c));
 		}
 
-		Func(const Func&) = default;
-		Func(Func&&) noexcept = default;
+		Function(const Function&) = default;
+		Function(Function&&) noexcept = default;
 
-		~Func() {
-				if (valid_) {
+		template <size_t S2, typename Enable = std::enable_if_t<S != S2>>
+		explicit Function(const Function<Ret(Args...), S2>& rhs) {
+				if constexpr (S < S2) {
+						if (rhs.size_ > S)
+								throw std::runtime_error("Object too large");
+				}
+				if (rhs.size_ != 0)
+						std::copy(rhs.buf_, rhs.buf_ + rhs.size_, buf_);
+				size_ = rhs.size_;
+		}
+
+		template <size_t S2, typename Enable = std::enable_if_t<S != S2>>
+		explicit Function(Function<Ret(Args...), S2>&& rhs) {
+				if constexpr (S < S2) {
+						if (rhs.size_ > S)
+								throw std::runtime_error("Object too large");
+				}
+				if (rhs.size_ != 0)
+						std::copy(rhs.buf_, rhs.buf_ + rhs.size_, buf_);
+				size_ = rhs.size_;
+				rhs.size_ = 0;
+		}
+
+		~Function() {
+				if (size_) {
 						detail::Callable<Ret, std::tuple<Args...>>* ptr =
 							(detail::Callable<Ret, std::tuple<Args...>>*)buf_;
 						ptr->~Callable();
 				}
 		}
 
-		Func& operator=(const Func&) = default;
-		Func& operator=(Func&&) noexcept = default;
+		Function& operator=(const Function&) = default;
+		Function& operator=(Function&&) noexcept = default;
+
+		template <size_t S2, typename Enable = std::enable_if_t<S != S2>>
+		Function& operator=(const Function<Ret(Args...), S2>& rhs) {
+				if constexpr (S < S2) {
+						if (rhs.size_ > S)
+								throw std::runtime_error("Object too large");
+				}
+				if (rhs.size_ != 0)
+						std::copy(rhs.buf_, rhs.buf_ + rhs.size_, buf_);
+				size_ = rhs.size_;
+				return *this;
+		}
+
+		template <size_t S2, typename Enable = std::enable_if_t<S != S2>>
+		Function& operator=(Function<Ret(Args...), S2>&& rhs) {
+				if constexpr (S < S2) {
+						if (rhs.size_ > S)
+								throw std::runtime_error("Object too large");
+				}
+				if (rhs.size_ != 0)
+						std::copy(rhs.buf_, rhs.buf_ + rhs.size_, buf_);
+				size_ = rhs.size_;
+				rhs.size_ = 0;
+				return *this;
+		}
 
 		Ret operator()(Args... args) {
-				if (!valid_) throw std::runtime_error("Func::operator()");
+				if (!size_) throw std::runtime_error("Func::operator()");
 				detail::Callable<Ret, std::tuple<Args...>>* ptr =
 					(detail::Callable<Ret, std::tuple<Args...>>*)buf_;
 				return (*ptr)(std::forward<Args>(args)...);
 		}
 
 		Ret operator()(Args... args) const {
-				if (!valid_) throw std::runtime_error("Func::operator()");
+				if (!size_) throw std::runtime_error("Func::operator()");
 				const detail::Callable<Ret, std::tuple<Args...>>* ptr =
 					(const detail::Callable<Ret, std::tuple<Args...>>*)buf_;
 				return (*ptr)(std::forward<Args>(args)...);
 		}
 
-		explicit operator bool() const { return valid_; }
+		explicit operator bool() const { return size_; }
 
 private:
-		char buf_[S] = {0};
-		bool valid_ = false;
+		size_t size_{0};
+		alignas(alignof(size_t)) char buf_[S] = {0};
 };
+
+namespace detail {
+
+template <typename C>
+struct function_deducer {};
+
+template <typename Ret, typename Class, typename... Args>
+struct function_deducer<Ret (Class::*)(Args...) const> {
+		using type = Ret(Args...);
+};
+template <typename Ret, typename Class, typename... Args>
+struct function_deducer<Ret (Class::*)(Args...)> {
+		using type = Ret(Args...);
+};
+}  // namespace detail
+
+template <typename Ret, typename... Args>
+Function(Ret (*)(Args...)) -> Function<Ret(Args...)>;
+
+template <typename C>
+Function(C) -> Function<
+	typename detail::function_deducer<decltype(&C::operator())>::type>;
+
+template <typename C>
+auto make_function(C&& c) {
+		return Function<
+			typename detail::function_deducer<decltype(&C::operator())>::type,
+			((sizeof(c) + 31) / 32) * 32>(std::forward<C>(c));
+}
+
 }  // namespace upp
--- unit-test/function/function.cpp
+++ unit-test/function/function.cpp
@@ -2,15 +2,40 @@
 
 #include <gtest/gtest.h>
 
+int func(double d, float f) { return d + f; }
 TEST(FunctionTest, Ctor) {
-		upp::Func<void()> f0{[]() {}};
-		upp::Func<int(int, double), 32> f1{
+		upp::Function f0{[]() {}};
+		upp::Function<int(int, double), 32> f1{
 			[](int i, double d) -> int { return i * d; }};
+		upp::Function f2{func};
+
+		auto f3 = upp::make_function([]() {});
 }
 
 TEST(FunctionTest, Invoke) {
 		int i = 0;
-		upp::Func<void(), 32> f0{[&]() { ++i; }};
+		upp::Function<void(), 32> f0{[&]() { ++i; }};
 		f0();
 		ASSERT_EQ(i, 1);
+}
+
+TEST(FunctionTest, SmallerToBiggerCtor) {
+		upp::Function<void(), 32> f0{[]() {}};
+		ASSERT_EQ(sizeof(f0) - sizeof(size_t), 32);
+		upp::Function<void(), 64> f1{std::move(f0)};
+		ASSERT_EQ(sizeof(f1) - sizeof(size_t), 64);
+}
+TEST(FunctionTest, SmallerToBiggerAssign) {
+		upp::Function<void(), 32> f0{[]() {}};
+		ASSERT_EQ(sizeof(f0) - sizeof(size_t), 32);
+		upp::Function<void(), 64> f1{};
+		f1 = std::move(f0);
+		ASSERT_EQ(sizeof(f1) - sizeof(size_t), 64);
+}
+
+TEST(FunctionTest, BiggerToSmaller) {
+		upp::Function<void(), 64> f0{[]() {}};
+		ASSERT_EQ(sizeof(f0) - sizeof(size_t), 64);
+		upp::Function<void()> f1{std::move(f0)};
+		ASSERT_EQ(sizeof(f1) - sizeof(size_t), 32);
 }
