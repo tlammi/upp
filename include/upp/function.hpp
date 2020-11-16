fe806b6f21a748651ebb509b1586d054ee948185
tlammi <tangojulietlima@gmail.com>
2020-11-16 20:37:45 +0200

    Documentation, refactoring, moving tests

--- include/upp/function.hpp
+++ include/upp/function.hpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <new>
 #include <stdexcept>
+#include <iostream>
 
 #include "upp/impl/traits/callable_prototype.hpp"
 
@@ -22,10 +23,29 @@ void do_dtor(void* blob){
 
 }// namespace detail
 
+
+/**
+ * \brief Generic callable template with statically allocated buffer
+ *
+ * Acts much like std::function except that upp::Function always uses
+ * an internal buffer for object storage and never allocates dynamically.
+ *
+ * The wrapped object's size is either statically or dynamically asserted.
+ * Direct assignation of a callable object, same Function specialization
+ * and smaller Function specializations can fail only statically.
+ * Functions with larger buffer size can be inserted into Functions with
+ * smaller buffer sizes but an exception may be thrown if the contained
+ * object is too large for the buffer of the target Function.
+ *
+ * \tparam C Wrapped callable type
+ * \tparam Size Static buffer size
+ * \tparam Align Static buffer alignment
+*/
 template<typename C, size_t Size=32, size_t Align=alignof(int)>
 class Function{};
 
 namespace detail {
+
 template <typename T>
 struct is_function : public std::false_type {};
 
@@ -33,6 +53,7 @@ template <typename Ret, size_t S, size_t A, typename... Args>
 struct is_function<Function<Ret(Args...), S, A>> : std::true_type {};
 
 }  // namespace detail
+
 
 template<typename Ret, size_t Size, size_t Align, typename... Args>
 class Function<Ret(Args...), Size, Align>{
@@ -42,30 +63,46 @@ class Function<Ret(Args...), Size, Align>{
 public:
 	using return_type = Ret;
 	static constexpr size_t max_size = Size;
-
 	typedef Ret(prototype_t)(Args...);
 
 	Function(){}
 
+	/**
+	 * \brief Construct a Function wrapping the given object
+	 *
+	 * \param c Wrapped object
+	 */
+	template<typename C,
+		typename = std::enable_if_t<!detail::is_function<C>::value>
+		>
+	explicit Function(C&& c){
+		init(std::forward<C>(c));
+	}
+	/**
+	 * \brief Assign a callable object
+	 */
 	template<typename C,
 		typename = std::enable_if_t<!detail::is_function<C>::value>>
-	Function(C&& c){
-		static_assert(sizeof(c) <= Size, "Object too large");
-		invoke_ = &detail::do_invoke<C, Ret, Args...>;
-		dtor_ = &detail::do_dtor<C>;
-		new(static_cast<C*>(static_cast<void*>(blob_)))C(std::forward<C>(c));
-		size_ = sizeof(c);
+	Function& operator=(C&& c){
+		init(std::forward<C>(c));
+		return *this;
 	}
 
+	/**
+	 * \brief Construct a function containing a function pointer
+	 *
+	 * \param func Function to wrap
+	 */
 	Function(Ret(*func)(Args...)){
 		static_assert(sizeof(func) <= Size, "Object too large");
 		invoke_ = &detail::do_invoke<Ret(*)(Args...), Ret, Args...>;
 		*static_cast<Ret(**)(Args...)>(static_cast<void*>(blob_)) = func;
-		size_ = 0;
+		size_ = sizeof(func);
 	}
 
-	Function(const Function&) = default;
-	Function& operator=(const Function&) = default;
+	Function(const Function&) = delete;
+	Function& operator=(const Function&) = delete;
+
 	Function(Function&& rhs) noexcept {
 		std::swap(size_, rhs.size_);
 		std::swap(invoke_, rhs.invoke_);
@@ -82,6 +119,11 @@ public:
 		return *this;
 	}
 
+	/**
+	 * \brief Move another Function specialization into the Function
+	 *
+	 * \throws std::runtime_error if the contained object is too large
+	 */
 	template<
 		typename OtherFunc,
 		typename = std::enable_if_t<detail::is_function<OtherFunc>::value>,
@@ -91,14 +133,36 @@ public:
 	Function(OtherFunc&& rhs){
 		if constexpr (OtherFunc::max_size > max_size)
 			if(rhs.size_ > max_size)
-				throw std::runtime_error("Function object too large");
-		
-		size_ = 0;
+				throw std::runtime_error("Function object too large");	
 		std::swap(size_, rhs.size_);
 		std::swap(invoke_, rhs.invoke_);
 		std::swap(dtor_, rhs.dtor_);
 		std::copy(rhs.blob_, rhs.blob_ + size_, blob_);
 	}
+	
+	/**
+	 * \brief Assign another Function specialization into the Function
+	 *
+	 * \throws std::runtime_error if the contained object is too large
+	 */
+	template<
+		typename OtherFunc,
+		typename = std::enable_if_t<detail::is_function<OtherFunc>::value>,
+		typename = std::enable_if_t<
+			!std::is_same_v<OtherFunc, Function<Ret(Args...), Size, Align>>>
+		>
+	Function& operator=(OtherFunc&& rhs){
+		if constexpr (OtherFunc::max_size > max_size)
+			if(rhs.size_ > max_size)
+				throw std::runtime_error("Function object too large");	
+		size_ = 0;
+		std::swap(size_, rhs.size_);
+		std::swap(invoke_, rhs.invoke_);
+		std::swap(dtor_, rhs.dtor_);
+		std::copy(rhs.blob_, rhs.blob_ + size_, blob_);
+		return *this;
+	}
+
 
 	template<
 		typename OtherFunc,
@@ -107,19 +171,44 @@ public:
 			OtherFunc, Function<Ret(Args...), Size, Align>>>
 		>
 	Function(const OtherFunc&) = delete;
+	template<
+		typename OtherFunc,
+		typename = std::enable_if_t<detail::is_function<OtherFunc>::value>,
+		typename = std::enable_if_t<!std::is_same_v<
+			OtherFunc, Function<Ret(Args...), Size, Align>>>
+		>
+	Function& operator=(const OtherFunc&) = delete;
 
 	~Function(){
 		if(size_ && dtor_)
 			dtor_(static_cast<void*>(blob_));
 	}
-
-
+		
+	/// Invoke the contained object
 	template<typename... Args2>
 	Ret operator()(Args2... args){
 		return invoke_(static_cast<void*>(blob_), std::forward<Args2>(args)...);
 	}
 	
+	/// Currently used buffer size
+	size_t used_size() const {
+		return size_;
+	}
+	
 private:
+	
+	/**
+	 * \brief Initialize all required fields
+	 */
+	template<typename C>
+	void init(C&& c){
+		static_assert(sizeof(std::decay_t<C>) <= Size, "Object too large");
+		invoke_ = &detail::do_invoke<std::decay_t<C>, Ret, Args...>;
+		dtor_ = &detail::do_dtor<std::decay_t<C>>;
+		new(static_cast<std::decay_t<C>*>(static_cast<void*>(blob_)))
+			std::decay_t<C>(std::forward<C>(c));
+		size_ = sizeof(std::decay_t<C>);
+	}
 	size_t size_{0};
 	Ret(*invoke_)(void*, Args...){nullptr};
 	void(*dtor_)(void*){nullptr};
@@ -127,10 +216,13 @@ private:
 };
 
 
-template<typename C, typename = std::enable_if_t<!std::is_lvalue_reference_v<C>>>
+template<typename C,
+	typename = std::enable_if_t<!std::is_lvalue_reference_v<C>>,
+	typename = std::enable_if_t<!detail::is_function<C>::value>>
 Function(C&&) -> Function<typename traits::callable_prototype<C>::type>;
 
-template<typename C>
+template<typename C,
+	typename = std::enable_if_t<!detail::is_function<C>::value>>
 Function(const C&) -> Function<typename traits::callable_prototype<C>::type>;
 
 }  // namespace upp
--- unit-test/function/function.cpp
+++ unit-test/function/function.cpp
@@ -1,6 +1,7 @@
 #include "upp/function.hpp"
 
 #include <gtest/gtest.h>
+#include <array>
 
 int func(double d, float f) { return d + f; }
 TEST(FunctionTest, Ctor) {
@@ -8,12 +9,22 @@ TEST(FunctionTest, Ctor) {
 		static_assert(
 			std::is_same_v<decltype(f0)::prototype_t, void()>
 		);
+		auto lambda0 = [](){};
+		ASSERT_EQ(f0.used_size(), sizeof(lambda0));
+
 		upp::Function<int(int, double), 32> f1{
 			[](int i, double d) -> int { return i * d; }};
+		static_assert(
+			std::is_same_v<decltype(f1)::prototype_t, int(int, double)>
+		);
+		auto lambda1 = [](int i, double d) -> int {return i*d;};
+		ASSERT_EQ(f1.used_size(), sizeof(lambda1));
+
 		upp::Function f2{func};
 		static_assert(
 			std::is_same_v<decltype(f2)::prototype_t, int(double, float)>
 		);
+		ASSERT_EQ(f2.used_size(), sizeof(&func));
 }
 
 TEST(FunctionTest, Invoke) {
@@ -42,4 +53,44 @@ TEST(FunctionTest, Assign) {
 		upp::Function<int(double), 32> f{
 			[](double d) -> int { return d + 100; }};
 		f = [](double d) -> int { return d * 100; };
+		auto lambda = [](double d) -> int{return d*100;};
+		ASSERT_EQ(f.used_size(), sizeof(lambda));
 }
+
+TEST(FunctionTest, TooBig){
+	auto lambda = [a = std::array<char, 36>()](double d) -> int{
+		return d;
+	};
+
+	upp::Function<int(double), 64> f{
+		std::move(lambda)
+	};
+
+	auto tmp = [](upp::Function<int(double), 64>&& f){
+		upp::Function<int(double), 32> f2{std::move(f)};
+	};
+
+	ASSERT_ANY_THROW(tmp(std::move(f)));
+}
+
+TEST(FunctionTest, Moving){
+	auto lambda = [a = std::array<char, 10>()](int i) -> int{
+		return i;
+	};
+	constexpr size_t lambda_size = sizeof(lambda);
+	upp::Function f0{std::move(lambda)};
+	ASSERT_EQ(f0.used_size(), lambda_size);
+	
+	
+	upp::Function f1{std::move(f0)};
+	ASSERT_EQ(f1.used_size(), lambda_size);
+
+	upp::Function<int(int), 64> f2{std::move(f1)};
+	ASSERT_EQ(f2.used_size(), lambda_size);
+	
+	f0 = std::move(f2);
+
+	ASSERT_EQ(f0.used_size(), lambda_size);
+
+}
+
