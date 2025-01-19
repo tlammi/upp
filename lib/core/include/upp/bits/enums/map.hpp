45ab3be1f25c57bad40622a046edbdae7ef466a2
Toni Lammi <tangojulietlima@gmail.com>
2025-01-19 17:04:22 +0200

    autoformat + constructing vector

--- lib/core/include/upp/bits/enums/map.hpp
+++ lib/core/include/upp/bits/enums/map.hpp
@@ -8,165 +8,210 @@ namespace upp {
 
 template <concepts::enum_type Key, class Val, class Store>
 class EnumMapBase {
-  Store m_data{};
-  size_t m_size{};
+    Store m_data{};
+    size_t m_size{};
 
  public:
-  template <bool Const>
-  class IteratorImpl {
-    friend class EnumMapBase;
+    template <bool Const>
+    class IteratorImpl {
+        friend class EnumMapBase;
 
-    using reference = std::conditional_t<Const, const Val &, Val &>;
-    using store_pointer = std::conditional_t<Const, const Store *, Store *>;
+     public:
+        using difference_type = typename std::ptrdiff_t;
+        using reference = std::conditional_t<Const, const Val&, Val&>;
+        using store_pointer = std::conditional_t<Const, const Store*, Store*>;
+        using value_type = std::pair<Key, Val>;
 
-    store_pointer m_store{};
-    union {
-      Key key{};
-      std::pair<Key, reference> pair;
-    } m_data{};
+     private:
+        store_pointer m_store{};
+        union {
+            Key key{};
+            std::pair<Key, reference> pair;
+        } m_data{};
 
-    constexpr bool is_end() const noexcept { return !m_store; }
+        constexpr bool is_end() const noexcept { return !m_store; }
 
-    constexpr size_t idx() noexcept {
-      return *magic_enum::enum_index(m_data.key);
-    }
-
-    constexpr void inc() noexcept {
-      auto i = idx() + 1;
-      if (i == magic_enum::enum_count<Key>()) {
-        m_store = nullptr;
-      } else {
-        m_data.key = magic_enum::enum_value<Key>(i);
-      }
-    }
-
-    constexpr IteratorImpl(Key key, store_pointer store) noexcept
-        : m_store(store), m_data{.key = key} {
-      assert(&m_data.key == &m_data.pair.first);
-      if (is_end()) return;
-      while (true) {
-        auto &optional = (*m_store)[idx()];
-        if (optional) {
-          std::construct_at(&m_data.pair, m_data.pair.first, *optional);
-          return;
+        constexpr size_t idx() noexcept {
+            return *magic_enum::enum_index(m_data.key);
         }
-        inc();
-        if (is_end()) return;
-      }
-    }
 
-   public:
-    constexpr IteratorImpl() noexcept = default;
-
-    constexpr bool operator==(const IteratorImpl &other) const noexcept {
-      if (!m_store && !other.m_store) return true;
-      return m_data.key == other.m_data.key;
-    }
-
-    constexpr IteratorImpl &operator++() noexcept {
-      while (true) {
-        inc();
-        if (is_end()) return *this;
-        auto &optional = (*m_store)[idx()];
-        if (optional) {
-          std::construct_at(&m_data.pair, m_data.pair.first, *optional);
-          return *this;
+        constexpr void inc() noexcept {
+            auto i = idx() + 1;
+            if (i == magic_enum::enum_count<Key>()) {
+                m_store = nullptr;
+            } else {
+                m_data.key = magic_enum::enum_value<Key>(i);
+            }
         }
-      }
+
+        constexpr IteratorImpl(Key key, store_pointer store) noexcept
+            : m_store(store), m_data{.key = key} {
+            assert(&m_data.key == &m_data.pair.first);
+            if (is_end()) return;
+            while (true) {
+                auto& optional = (*m_store)[idx()];
+                if (optional) {
+                    std::construct_at(&m_data.pair, m_data.pair.first,
+                                      *optional);
+                    return;
+                }
+                inc();
+                if (is_end()) return;
+            }
+        }
+
+     public:
+        constexpr IteratorImpl() noexcept = default;
+
+        constexpr IteratorImpl(const IteratorImpl& other) noexcept
+            : m_store(other.m_store) {
+            if (m_store) std::construct_at(&m_data.pair, other.m_data.pair);
+        }
+
+        constexpr IteratorImpl& operator=(const IteratorImpl& other) noexcept {
+            m_store = other.m_store;
+            std::construct_at(&m_data.pair, other.m_data.pair);
+            return *this;
+        }
+
+        constexpr IteratorImpl(IteratorImpl&& other) noexcept
+            : m_store(other.m_store) {
+            std::construct_at(&m_data.pair, other.pair);
+        }
+
+        constexpr IteratorImpl& operator=(IteratorImpl&& other) noexcept {
+            m_store = other.m_store;
+            std::construct_at(&m_data.pair, other.pair);
+            return *this;
+        }
+
+        constexpr ~IteratorImpl() = default;
+
+        constexpr bool operator==(const IteratorImpl& other) const noexcept {
+            if (!m_store && !other.m_store) return true;
+            if (m_store && other.m_store) {
+                return m_data.key == other.m_data.key;
+            }
+            return false;
+        }
+
+        constexpr IteratorImpl& operator++() noexcept {
+            while (true) {
+                inc();
+                if (is_end()) return *this;
+                auto& optional = (*m_store)[idx()];
+                if (optional) {
+                    std::construct_at(&m_data.pair, m_data.pair.first,
+                                      *optional);
+                    return *this;
+                }
+            }
+        }
+
+        constexpr IteratorImpl operator++(int) noexcept {
+            auto cpy = *this;
+            ++(*this);
+            return cpy;
+        }
+
+        template <class S>
+        constexpr decltype(auto) operator*(this S&& self) noexcept {
+            return std::forward<S>(self).m_data.pair;
+        }
+
+        constexpr std::pair<Key, reference>* operator->() noexcept {
+            return &m_data.pair;
+        }
+
+        constexpr const std::pair<Key, reference>* operator->() const noexcept {
+            return &m_data.pair;
+        }
+    };
+
+    using key_type = Key;
+    using mapped_type = Val;
+    using value_type = std::pair<key_type, mapped_type>;
+    using iterator = IteratorImpl<false>;
+    using const_iterator = IteratorImpl<true>;
+
+    static constexpr size_t map_capacity = magic_enum::enum_count<Key>();
+
+    constexpr size_t capacity() const noexcept { return map_capacity; }
+    constexpr size_t size() const noexcept { return m_size; }
+    [[nodiscard]] constexpr bool empty() const noexcept { return !m_size; }
+
+    mapped_type& operator[](Key key) {
+        auto& elem = m_data[*magic_enum::enum_index(key)];
+        if (!elem) {
+            elem.emplace();
+            ++m_size;
+        }
+        return *elem;
     }
 
-    constexpr std::pair<Key, reference> &operator*() noexcept {
-      return m_data.pair;
+    mapped_type& at(Key key) {
+        auto& elem = m_data[*magic_enum::enum_index(key)];
+        return elem.value();
     }
-    constexpr std::pair<Key, reference> *operator->() noexcept {
-      return &m_data.pair;
+
+    const mapped_type& at(Key key) const {
+        auto& elem = m_data[*magic_enum::enum_index(key)];
+        return elem.value();
     }
-  };
 
-  using key_type = Key;
-  using mapped_type = Val;
-  using value_type = std::pair<key_type, mapped_type>;
-  using iterator = IteratorImpl<false>;
-  using const_iterator = IteratorImpl<true>;
-
-  static constexpr size_t map_capacity = magic_enum::enum_count<Key>();
-
-  constexpr size_t capacity() const noexcept { return map_capacity; }
-  constexpr size_t size() const noexcept { return m_size; }
-  [[nodiscard]] constexpr bool empty() const noexcept { return !m_size; }
-
-  mapped_type &operator[](Key key) {
-    auto &elem = m_data[*magic_enum::enum_index(key)];
-    if (!elem) {
-      elem.emplace();
-      ++m_size;
+    iterator begin() noexcept {
+        return {magic_enum::enum_value<Key>(0), &m_data};
     }
-    return *elem;
-  }
 
-  mapped_type &at(Key key) {
-    auto &elem = m_data[*magic_enum::enum_index(key)];
-    return elem.value();
-  }
-
-  const mapped_type &at(Key key) const {
-    auto &elem = m_data[*magic_enum::enum_index(key)];
-    return elem.value();
-  }
-
-  iterator begin() noexcept {
-    return {magic_enum::enum_value<Key>(0), &m_data};
-  }
-
-  const_iterator begin() const noexcept {
-    return {magic_enum::enum_value<Key>(0), &m_data};
-  }
-
-  iterator end() noexcept { return {}; }
-  const_iterator end() const noexcept { return {}; }
-
-  size_t erase(Key key) noexcept {
-    auto &opt = m_data[*magic_enum::enum_index(key)];
-    if (opt) {
-      opt.reset();
-      --m_size;
-      return 1;
+    const_iterator begin() const noexcept {
+        return {magic_enum::enum_value<Key>(0), &m_data};
     }
-    return 0;
-  }
+
+    iterator end() noexcept { return {}; }
+    const_iterator end() const noexcept { return {}; }
+
+    size_t erase(Key key) noexcept {
+        auto& opt = m_data[*magic_enum::enum_index(key)];
+        if (opt) {
+            opt.reset();
+            --m_size;
+            return 1;
+        }
+        return 0;
+    }
 
  protected:
-  constexpr EnumMapBase(Store init) : m_data(std::move(init)) {}
-  constexpr EnumMapBase() = default;
+    constexpr EnumMapBase(Store init) : m_data(std::move(init)) {}
+    constexpr EnumMapBase() = default;
 
-  constexpr EnumMapBase(Store init, std::initializer_list<value_type> list)
-      : m_data(std::move(init)) {
-    for (auto &&pair : list) {
-      operator[](pair.first) = std::move(pair.second);
+    constexpr EnumMapBase(Store init, std::initializer_list<value_type> list)
+        : m_data(std::move(init)) {
+        for (auto&& pair : list) {
+            operator[](pair.first) = std::move(pair.second);
+        }
     }
-  }
-  constexpr EnumMapBase(std::initializer_list<value_type> list) {
-    for (auto &&pair : list) {
-      operator[](pair.first) = std::move(pair.second);
+    constexpr EnumMapBase(std::initializer_list<value_type> list) {
+        for (auto&& pair : list) {
+            operator[](pair.first) = std::move(pair.second);
+        }
     }
-  }
 
-  constexpr ~EnumMapBase() = default;
-};
+    constexpr ~EnumMapBase() = default;
+};  // namespace upp
 
 template <concepts::enum_type Key, class Val>
 class EnumMap
     : public EnumMapBase<Key, Val, std::unique_ptr<std::optional<Val>[]>> {
-  using Base = EnumMapBase<Key, Val, std::unique_ptr<std::optional<Val>[]>>;
+    using Base = EnumMapBase<Key, Val, std::unique_ptr<std::optional<Val>[]>>;
 
  public:
-  constexpr EnumMap()
-      : Base(std::unique_ptr<std::optional<Val>[]>(
-            new std::optional<Val>[Base::map_capacity])) {}
-  constexpr EnumMap(std::initializer_list<typename Base::value_type> list)
-      : Base(std::unique_ptr<std::optional<Val>[]>(
-                 new std::optional<Val>[Base::map_capacity]),
-             list) {}
+    constexpr EnumMap()
+        : Base(std::unique_ptr<std::optional<Val>[]>(
+              new std::optional<Val>[Base::map_capacity])) {}
+    constexpr EnumMap(std::initializer_list<typename Base::value_type> list)
+        : Base(std::unique_ptr<std::optional<Val>[]>(
+                   new std::optional<Val>[Base::map_capacity]),
+               list) {}
 };
 
 template <concepts::enum_type Key, class Val>
@@ -174,13 +219,15 @@ class EnumMapStatic
     : public EnumMapBase<
           Key, Val,
           std::array<std::optional<Val>, magic_enum::enum_count<Key>()>> {
-  using Base = EnumMapBase<
-      Key, Val, std::array<std::optional<Val>, magic_enum::enum_count<Key>()>>;
+    using Base = EnumMapBase<
+        Key, Val,
+        std::array<std::optional<Val>, magic_enum::enum_count<Key>()>>;
 
  public:
-  constexpr EnumMapStatic() : Base() {}
-  constexpr EnumMapStatic(std::initializer_list<typename Base::value_type> list)
-      : Base(list) {}
+    constexpr EnumMapStatic() : Base() {}
+    constexpr EnumMapStatic(
+        std::initializer_list<typename Base::value_type> list)
+        : Base(list) {}
 };
 
 }  // namespace upp
--- test/unit/core/enums.cpp
+++ test/unit/core/enums.cpp
@@ -4,109 +4,125 @@
 #include <upp/enums.hpp>
 
 enum class Int : int {
-  A = 0,
-  B = 1,
+    A = 0,
+    B = 1,
 };
 
+static_assert(std::forward_iterator<upp::EnumMap<Int, int>::iterator>);
+
 TEST(Cast, ToUnderlying) {
-  ASSERT_EQ(upp::underlying_cast(Int::A), 0);
-  ASSERT_EQ(upp::underlying_cast(Int::B), 1);
+    ASSERT_EQ(upp::underlying_cast(Int::A), 0);
+    ASSERT_EQ(upp::underlying_cast(Int::B), 1);
 }
 
 TEST(Cast, FromUnderlying) {
-  ASSERT_EQ(upp::underlying_cast<Int>(0), Int::A);
-  ASSERT_EQ(upp::underlying_cast<Int>(1), Int::B);
+    ASSERT_EQ(upp::underlying_cast<Int>(0), Int::A);
+    ASSERT_EQ(upp::underlying_cast<Int>(1), Int::B);
 }
 
 TEST(Map, DefaultCtor) {
-  auto map = upp::EnumMap<Int, int>();
-  ASSERT_EQ(map.capacity(), 2);
-  ASSERT_EQ(map.size(), 0);
+    auto map = upp::EnumMap<Int, int>();
+    ASSERT_EQ(map.capacity(), 2);
+    ASSERT_EQ(map.size(), 0);
 }
 
 TEST(Map, InitializerList) {
-  auto map = upp::EnumMap<Int, int>{{Int::A, 10}, {Int::B, 20}};
+    auto map = upp::EnumMap<Int, int>{{Int::A, 10}, {Int::B, 20}};
 }
 
 TEST(Map, Add) {
-  auto map = upp::EnumMap<Int, int>{};
-  map[Int::A] = 1;
-  ASSERT_EQ(map.size(), 1);
-  ASSERT_EQ(map.at(Int::A), 1);
-  ASSERT_EQ(std::as_const(map).at(Int::A), 1);
+    auto map = upp::EnumMap<Int, int>{};
+    map[Int::A] = 1;
+    ASSERT_EQ(map.size(), 1);
+    ASSERT_EQ(map.at(Int::A), 1);
+    ASSERT_EQ(std::as_const(map).at(Int::A), 1);
 }
 
 TEST(Map, StaticAdd) {
-  auto map = upp::EnumMapStatic<Int, int>{};
-  map[Int::A] = 1;
-  ASSERT_EQ(map.size(), 1);
-  ASSERT_EQ(map.at(Int::A), 1);
-  ASSERT_EQ(std::as_const(map).at(Int::A), 1);
+    auto map = upp::EnumMapStatic<Int, int>{};
+    map[Int::A] = 1;
+    ASSERT_EQ(map.size(), 1);
+    ASSERT_EQ(map.at(Int::A), 1);
+    ASSERT_EQ(std::as_const(map).at(Int::A), 1);
 }
 
 TEST(Map, Iterate) {
-  enum class Enum {
-    A,
-    B,
-    C,
-    D,
-  };
+    enum class Enum {
+        A,
+        B,
+        C,
+        D,
+    };
 
-  auto map = upp::EnumMap<Enum, std::string>();
-  map[Enum::A] = "a";
-  map[Enum::B] = "b";
-  map[Enum::D] = "d";
+    auto map = upp::EnumMap<Enum, std::string>();
+    map[Enum::A] = "a";
+    map[Enum::B] = "b";
+    map[Enum::D] = "d";
 
-  auto iter = map.begin();
-  ASSERT_EQ(iter->first, Enum::A);
-  ASSERT_EQ(iter->second, "a");
-  iter->second = "A";
-  ASSERT_EQ(map[Enum::A], "A");
-  ++iter;
-  ASSERT_EQ(iter->first, Enum::B);
-  ASSERT_EQ(iter->second, "b");
-  iter->second = "B";
-  ASSERT_EQ(map[Enum::B], "B");
-  ++iter;
-  ASSERT_EQ(iter->first, Enum::D);
-  ASSERT_EQ(iter->second, "d");
-  iter->second = "D";
-  ASSERT_EQ(map[Enum::D], "D");
-  ++iter;
-  ASSERT_EQ(iter, map.end());
+    auto iter = map.begin();
+    ASSERT_EQ(iter->first, Enum::A);
+    ASSERT_EQ(iter->second, "a");
+    iter->second = "A";
+    ASSERT_EQ(map[Enum::A], "A");
+    ++iter;
+    ASSERT_EQ(iter->first, Enum::B);
+    ASSERT_EQ(iter->second, "b");
+    iter->second = "B";
+    ASSERT_EQ(map[Enum::B], "B");
+    ++iter;
+    ASSERT_EQ(iter->first, Enum::D);
+    ASSERT_EQ(iter->second, "d");
+    iter->second = "D";
+    ASSERT_EQ(map[Enum::D], "D");
+    ++iter;
+    ASSERT_EQ(iter, map.end());
 }
 
 TEST(Map, ConstIterate) {
-  enum class Enum {
-    A,
-    B,
-    C,
-  };
-  const auto map =
-      upp::EnumMap<Enum, std::string>{{Enum::A, "a"}, {Enum::C, "c"}};
+    enum class Enum {
+        A,
+        B,
+        C,
+    };
+    const auto map =
+        upp::EnumMap<Enum, std::string>{{Enum::A, "a"}, {Enum::C, "c"}};
 
-  auto iter = map.begin();
-  ASSERT_EQ(iter->first, Enum::A);
-  ASSERT_EQ(iter->second, "a");
-  ++iter;
-  ASSERT_EQ(iter->first, Enum::C);
-  ASSERT_EQ(iter->second, "c");
-  ++iter;
-  ASSERT_EQ(iter, map.end());
+    auto iter = map.begin();
+    ASSERT_EQ(iter->first, Enum::A);
+    ASSERT_EQ(iter->second, "a");
+    ++iter;
+    ASSERT_EQ(iter->first, Enum::C);
+    ASSERT_EQ(iter->second, "c");
+    ++iter;
+    ASSERT_EQ(iter, map.end());
 }
 
 TEST(Map, EraseOne) {
-  auto map = upp::EnumMap<Int, int>();
-  map[Int::A] = 1;
-  auto count = map.erase(Int::A);
-  ASSERT_EQ(count, 1);
-  ASSERT_TRUE(map.empty());
+    auto map = upp::EnumMap<Int, int>();
+    map[Int::A] = 1;
+    auto count = map.erase(Int::A);
+    ASSERT_EQ(count, 1);
+    ASSERT_TRUE(map.empty());
 }
 
 TEST(Map, EraseMiss) {
-  auto map = upp::EnumMap<Int, int>();
-  map[Int::A] = 1;
-  auto count = map.erase(Int::B);
-  ASSERT_EQ(count, 0);
-  ASSERT_EQ(map.size(), 1);
+    auto map = upp::EnumMap<Int, int>();
+    map[Int::A] = 1;
+    auto count = map.erase(Int::B);
+    ASSERT_EQ(count, 0);
+    ASSERT_EQ(map.size(), 1);
+}
+
+TEST(Map, CreateVec) {
+    enum class Enum {
+        A,
+        B,
+        C,
+    };
+    using enum Enum;
+    auto map = upp::EnumMap<Enum, std::string>{{A, "a"}, {B, "b"}, {C, "c"}};
+    auto vec =
+        std::vector<std::pair<Enum, std::string>>(map.begin(), map.end());
+
+    ASSERT_EQ(vec.size(), 3);
 }
