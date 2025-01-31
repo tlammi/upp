5c569f66653f09f06d0a6a0e341750cfa13b99b5
Toni Lammi <tangojulietlima@gmail.com>
2025-01-31 18:48:38 +0200

    check whether an enum is a bitmask

--- lib/core/include/upp/bits/concepts.hpp
+++ lib/core/include/upp/bits/concepts.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <magic_enum/magic_enum.hpp>
+#include <magic_enum/magic_enum_utility.hpp>
 #include <type_traits>
 
 namespace upp::concepts {
@@ -17,6 +19,35 @@ struct prototype_impl<
 }  // namespace detail
 template <class T>
 concept enum_type = std::is_enum_v<T>;
+
+namespace detail {
+
+template <enum_type E>
+constexpr bool is_enum_bitmask()
+    requires(magic_enum::enum_count<E>() > 0)
+{
+    auto mask = std::underlying_type_t<E>{};
+    bool success = true;
+    magic_enum::enum_for_each<E>([&](E v) {
+        if (!success) return;
+        auto u = static_cast<std::underlying_type_t<E>>(v);
+        if (u & mask) success = false;
+        mask |= u;
+    });
+    return success;
+}
+
+template <concepts::enum_type E>
+constexpr bool is_enum_bitmask()
+    requires(magic_enum::enum_count<E>() == 0)
+{
+    return true;
+}
+
+}  // namespace detail
+
+template <class E>
+concept enum_bitmask = enum_type<E> && detail::is_enum_bitmask<E>();
 
 template <class T, class F>
 concept prototype = detail::prototype_impl<T, F>::value;
--- test/unit/core/concepts.cpp
+++ test/unit/core/concepts.cpp
@@ -14,3 +14,19 @@ static_assert(upp::concepts::prototype<decltype(bar), void()>);
 static_assert(upp::concepts::prototype<decltype([] {}), void()>);
 static_assert(!upp::concepts::prototype<decltype([] {}), int()>);
 static_assert(!upp::concepts::prototype<decltype([] {}), void(int)>);
+
+enum class BitMask {
+    A = 1,
+    B = 1 << 1,
+    C = 1 << 2,
+};
+
+static_assert(upp::concepts::enum_bitmask<BitMask>);
+
+enum class NotBitMask {
+    A = 1,
+    B = 2,
+    C = 3,
+};
+static_assert(!upp::concepts::enum_bitmask<NotBitMask>);
+
