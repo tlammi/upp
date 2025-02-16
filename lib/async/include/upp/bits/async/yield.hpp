201b7e3f327cbaa284cc6db5135349586c97abb8
Toni Lammi <tangojulietlima@gmail.com>
2025-02-16 08:29:38 +0200

    asserts

--- lib/async/include/upp/bits/async/yield.hpp
+++ lib/async/include/upp/bits/async/yield.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <coroutine>
+#include <upp/util.hpp>
 
 namespace upp::async {
 
 class Yielder {
+    UPP_ASSERT_CONDITION_NO_UNIQUE_ADDRESS mutable AssertCondition m_used{
+        "upp::async::yield() not awaited"};
+
  public:
-    constexpr bool await_ready() const noexcept { return false; }
+    constexpr bool await_ready() const noexcept {
+        m_used = true;
+        return false;
+    }
 
     constexpr void await_suspend(
         std::coroutine_handle<> /*unused*/) const noexcept {}
@@ -16,6 +23,6 @@ class Yielder {
  private:
 };
 
-constexpr Yielder yield() { return {}; }
+[[nodiscard]] constexpr Yielder yield() { return {}; }
 
 }  // namespace upp::async
